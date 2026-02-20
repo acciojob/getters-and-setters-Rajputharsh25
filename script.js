f4a9fb3572f3df9a70861eabb786564b610b607a
Rajputharsh25 <144730809+Rajputharsh25@users.noreply.github.com>
2026-02-20 19:09:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,68 +1,38 @@
-// Person Class
 class Person {
-    constructor(name, age) {
-        this._name = name;
-        this._age = age;
-    }
+  constructor(name, age) {
+    this._name = name;
+    this._age = age;
+  }
 
-    // Getter for name
-    get name() {
-        return this._name;
-    }
+  // Getter for name
+  get name() {
+    return this._name;
+  }
 
-    // Getter for age
-    get age() {
-        return this._age;
-    }
+  // Getter for age (needed to retrieve the value after using the setter)
+  get age() {
+    return this._age;
+  }
 
-    // Setter for age
-    set age(age) {
-        this._age = age;
-    }
+  // Setter for age
+  set age(newAge) {
+    this._age = newAge;
+  }
 }
 
-
-// Student Class (inherits Person)
 class Student extends Person {
-    constructor(name, age) {
-        super(name, age);
-    }
-
-    // study method
-    study() {
-        console.log(`${this.name} is studying`);
-    }
+  study() {
+    console.log(`${this.name} is studying`);
+  }
 }
 
-
-// Teacher Class (inherits Person)
 class Teacher extends Person {
-    constructor(name, age) {
-        super(name, age);
-    }
-
-    // teach method
-    teach() {
-        console.log(`${this.name} is teaching`);
-    }
+  teach() {
+    console.log(`${this.name} is teaching`);
+  }
 }
 
-
-// Example usage
-
-// Person
-const person = new Person("John", 25);
-console.log(person.name);  // John
-
-person.age = 30;
-console.log(person.age);   // 30
-
-
-// Student
-const student = new Student("Alice", 22);
-student.study();  // Alice is studying
-
-
-// Teacher
-const teacher = new Teacher("Bob", 40);
-teacher.teach();  // Bob is teaching
+// Attach to window for Cypress tests
+window.Person = Person;
+window.Student = Student;
+window.Teacher = Teacher;
