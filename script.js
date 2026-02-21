95f486559cd636a9fd65cd66e5911c1190ce06ab
Rajputharsh25 <144730809+Rajputharsh25@users.noreply.github.com>
2026-02-21 20:10:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Person class
 class Person {
   constructor(name, age) {
     this._name = name;
@@ -9,7 +10,7 @@ class Person {
     return this._name;
   }
 
-  // Getter for age (needed to retrieve the value after using the setter)
+  // Getter for age
   get age() {
     return this._age;
   }
@@ -20,19 +21,49 @@ class Person {
   }
 }
 
+
+// Student class
 class Student extends Person {
+  constructor(name, age) {
+    super(name, age);
+  }
+
   study() {
     console.log(`${this.name} is studying`);
   }
 }
 
+
+// Teacher class
 class Teacher extends Person {
+  constructor(name, age) {
+    super(name, age);
+  }
+
   teach() {
     console.log(`${this.name} is teaching`);
   }
 }
 
-// Attach to window for Cypress tests
-window.Person = Person;
-window.Student = Student;
-window.Teacher = Teacher;
+
+// Function to run tests
+function runTest() {
+
+  // Person test
+  const person = new Person("John", 25);
+  console.log(person.name); // John
+
+  person.age = 30;
+  console.log(person.age); // 30
+
+
+  // Student test
+  const student = new Student("Alice", 22);
+  student.study(); // Alice is studying
+
+
+  // Teacher test
+  const teacher = new Teacher("Bob", 40);
+  teacher.teach(); // Bob is teaching
+
+}
